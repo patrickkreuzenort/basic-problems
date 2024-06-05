967421732cd55f7c59e7a2148d6dbd0671b7aa2a
Karolina <karolina@MacBook-Air-Karolina.local>
2024-06-05 21:02:31 +0200

    Dijkstra

---
+++ Algorithms/src/main/java/Dijkstra.java
@@ -0,0 +1,175 @@
+import java.util.*;
+
+/**
+ * Shortest Path Problem with Dijkstra.
+ * The emphasis in the example is the shortest path problem (SPP),
+ * being one of the fundamental theoretic problems known in graph theory.
+ */
+public class Dijkstra {
+
+    /**
+     * Given a positively weighted graph and a starting node,
+     * we determine the shortest path and distance from the source
+     * to all destinations.
+     * The core idea of the Dijkstra algorithm is to continuously eliminate
+     * longer paths between the starting node and all possible destinations.
+     * To keep track of the process, we need to have two distinct sets of nodes:
+     * settled and unsettled.
+     * Settled nodes are the ones with a known minimum distance from the source.
+     * Steps to solve the shortest path problem with Dijkstra:
+     * 1. Set distance to startNode to zero.
+     * 2. Set all distances of remaining to an infinite value.
+     * 3. Add the startNode to the unsettled nodes. Settled node are still empty.
+     * 4. While the unsettled nodes set is not empty:
+     * 4.1. Choose an evaluation node from the unsettled nodes set
+     * The evaluation node should be the one with the lowest distance from the source.
+     * 4.2. Calculate new distances to direct neighbours by keeping the lowest distance at each evaluation.
+     * 4.3 Add neighbours that are not yet settled to the unsettled nodes set.
+     */
+
+
+    public static void main(String[] args) {
+
+        Node nodeA = new Node("A");
+        Node nodeB = new Node("B");
+        Node nodeC = new Node("C");
+        Node nodeD = new Node("D");
+        Node nodeE = new Node("E");
+        Node nodeF = new Node("F");
+
+        nodeA.addDestination(nodeB, 10);
+        nodeA.addDestination(nodeC, 15);
+
+        nodeB.addDestination(nodeD, 12);
+        nodeB.addDestination(nodeF, 15);
+
+        nodeC.addDestination(nodeE, 10);
+
+        nodeD.addDestination(nodeE, 2);
+        nodeD.addDestination(nodeF, 1);
+
+        nodeF.addDestination(nodeE, 5);
+
+        Graph graph = new Graph();
+
+        graph.addNode(nodeA);
+        graph.addNode(nodeB);
+        graph.addNode(nodeC);
+        graph.addNode(nodeD);
+        graph.addNode(nodeE);
+        graph.addNode(nodeF);
+
+        graph = Dijkstra.calculateShortestPathFromSource(graph, nodeA);
+    }
+
+
+    static class Graph {
+
+        private Set<Node> nodes = new HashSet<>();
+
+        public void addNode(Node node) {
+            nodes.add(node);
+        }
+
+    }
+
+    static class Node {
+
+        private String name;
+
+        private List<Node> shortestPath = new LinkedList<>();
+
+        private Integer distance = Integer.MAX_VALUE;
+
+        private Map<Node, Integer> adjacentNodes = new HashMap<>();
+
+        public void addDestination(Node destination, int distance) {
+            adjacentNodes.put(destination, distance);
+        }
+
+        public Node(String name) {
+            this.name = name;
+        }
+
+        public Map<Node, Integer> getAdjacentNodes() {
+            return adjacentNodes;
+        }
+
+        public void setAdjacentNodes(Map<Node, Integer> adjacentNodes) {
+            this.adjacentNodes = adjacentNodes;
+        }
+
+        public List<Node> getShortestPath() {
+            return shortestPath;
+        }
+
+        public void setShortestPath(List<Node> shortestPath) {
+            this.shortestPath = shortestPath;
+        }
+
+        public Integer getDistance() {
+            return distance;
+        }
+
+        public void setDistance(Integer distance) {
+            this.distance = distance;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    static Graph calculateShortestPathFromSource(Graph graph, Node source) {
+        source.setDistance(0);
+
+        Set<Node> settledNodes = new HashSet<>();
+        Set<Node> unsettledNodes = new HashSet<>();
+
+        unsettledNodes.add(source);
+
+        while (!unsettledNodes.isEmpty()) {
+            Node currentNode = getLowestDistanceNode(unsettledNodes);
+            unsettledNodes.remove(currentNode);
+            for (Map.Entry<Node, Integer> adjacencyPair :
+                    currentNode.getAdjacentNodes().entrySet()) {
+                Node adjacentNode = adjacencyPair.getKey();
+                Integer edgeWeight = adjacencyPair.getValue();
+                if (!settledNodes.contains(adjacentNode)) {
+                    calculateMinimumDistance(adjacentNode, edgeWeight, currentNode);
+                    unsettledNodes.add(adjacentNode);
+                }
+            }
+            settledNodes.add(currentNode);
+        }
+        return graph;
+    }
+
+    private static Node getLowestDistanceNode(Set<Node> unsettledNodes) {
+        Node lowestDistanceNode = null;
+        int lowestDistance = Integer.MAX_VALUE;
+        for (Node node : unsettledNodes) {
+            int nodeDistance = node.getDistance();
+            if (nodeDistance < lowestDistance) {
+                lowestDistance = nodeDistance;
+                lowestDistanceNode = node;
+            }
+        }
+        return lowestDistanceNode;
+    }
+
+
+    private static void calculateMinimumDistance(Node evaluationNode, Integer edgeWeigh, Node sourceNode) {
+        Integer sourceDistance = sourceNode.getDistance();
+        if (sourceDistance + edgeWeigh < evaluationNode.getDistance()) {
+            evaluationNode.setDistance(sourceDistance + edgeWeigh);
+            LinkedList<Node> shortestPath = new LinkedList<>(sourceNode.getShortestPath());
+            shortestPath.add(sourceNode);
+            evaluationNode.setShortestPath(shortestPath);
+        }
+    }
+}
